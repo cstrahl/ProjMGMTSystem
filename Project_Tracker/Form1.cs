ee27c326836ceec518db86a369e0040759e6a4c5
bthayer3 <btwildcat13@gmail.com>
2018-11-13 18:18:40 -0500

    Minor fixes

--- Project_Tracker/Form1.cs
+++ Project_Tracker/Form1.cs
@@ -12,11 +12,17 @@ namespace Project_Tracker
 {
     public partial class Main_Form : Form
     {
-
+        private List<ProjectData> ProjectList = new List<ProjectData>();
 
         public Main_Form()
         {
             InitializeComponent();
+
+            ProjectData x = new ProjectData();
+            
+
+            Risk y = new Risk();
+            
         }
 
         private void Add_Button_Click(object sender, EventArgs e)
--- Project_Tracker/ProjectData.cs
+++ Project_Tracker/ProjectData.cs
@@ -9,20 +9,16 @@ namespace Project_Tracker
 
     class ProjectData
     {
-
-        private string projName { get; set; }
+        public string projName { get; set; }
         private object projManager { get; set; }
         private string projDescrip { get; set; }
-        private List<string> projMembers = new List<string>();
-        private List<string> projRisk = new List<string>();
-        private List<string> projReq = new List<string>();
+        private List<Person> projMembers = new List<Person>();
+        private List<Risk> projRisk = new List<Risk>();
+        private List<Requirement> projReq = new List<Requirement>();
 
-        //private object[] projMembers { get; set; }
-        //private object[] projRisk { get; set; }
-        //private object[] projReq { get; set; }
+        public ProjectData() { }
 
-
-        public ProjectData(string projName, object projManager, string projDescrip, List<string> projMembers, List<string> projRisk, List<string> projReq)
+        public ProjectData(string projName, object projManager, string projDescrip, List<Person> projMembers, List<Risk> projRisk, List<Requirement> projReq)
         {
             this.projName = projName;
             this.projManager = projManager;
