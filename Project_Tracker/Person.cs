786e5fa549cb8bd561e0b754c7ab9db28a4ea7f6
Chris Strahl <christiandstrahl@gmail.com>
2018-11-15 17:34:25 -0500

    getset person fix

--- Project_Tracker/Person.cs
+++ Project_Tracker/Person.cs
@@ -3,14 +3,29 @@
 namespace Project_Tracker{
 
     class Person{
-        private string firstName { get; set; }
-        private string lastName { get; set; }
+        private string firstName;
+        private string lastName;
 
         public Person(string firstName, string lastName){
             this.firstName = firstName;
             this.lastName = lastName;
         }
 
+        protected void setFirstName ( string firstName){
+            this.firstName = firstName;
+        }
+
+        protected void setLastName ( string lastName){
+            this.lastName = lastName;
+        }
+
+        public string getFirstName(){
+            return firstName;
+        }
+
+        public string getLastName(){
+            return lastName;
+        }
 
         public override string ToString(){
             return (firstName + " " + lastName);
